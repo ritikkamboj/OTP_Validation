4e67a50e8299839d36465a6dee127c0451dab9eb
Aashish_kumar2 <ritikkamboj4314@gmail.com>
2025-04-07 22:57:02 +0530

    created input boxes and working on onChnage on input boxes

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from "react";
 
+const OTP_DIGITS_COUNT = 5;
 function App() {
-  const [count, setCount] = useState(0)
+  const [inputArray, setInputArray] = useState(
+    new Array(OTP_DIGITS_COUNT).fill("")
+  );
+  console.log(inputArray);
 
+  const handleChange = (value, index) => {
+    console.log(value, index);
+    // inputArray[index] = value
+    // console.log(inputArray)
+    if(isNaN(value))
+    {
+      return;
+    }
+
+    const newArr = [...inputArray];
+    newArr[index] = value;
+    setInputArray(newArr);
+  };
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="app">
+      <h1>OTP Validator </h1>
+
+      {inputArray.map((input, index) => (
+        <input
+          className="otp-input"
+          key={index}
+          type="text"
+          value={input}
+          onChange={(e) => handleChange(e.target.value, index)}
+        />
+      ))}
+    </div>
+  );
 }
 
-export default App
+export default App;
